a5419601a0bf00e09229b5c806931514dc7d3b42
Nishant4coding <srivastava4nishant@gmail.com>
2024-05-04 08:01:13 +0530

    fix navbar

--- components/sections/Nav/index.tsx
+++ components/sections/Nav/index.tsx
@@ -7,11 +7,11 @@ import logo from "@/public/mlogo.png";
 
 function Nav() {
   return (
-    <div className="fc fcc w-full z-10000000  top-0 left-0 bg-black bg-opacity-50 backdrop-blur-md">
-      <div className="fr fcb md:w-[90%] w-[95%]">
+    <div className="fc fcc w-full z-10000000 fixed top-0 left-0 bg-black bg-opacity-50 backdrop-blur-md">
+      <div className="fr fcb md:w-[90%] w-[95%] z-50">
         <div className="fr">
           <Image
-            className="w-16 h-16"
+            className="w-16 h-16 backdrop-blur-md"
             alt="main logo"
             src={logo}
             width={100}
